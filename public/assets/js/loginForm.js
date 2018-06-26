8b41e8ce7d7cca62dc2c9f63eedaf47b8536f0fe
Lina Shamoon <shamoonlina@gmail.com>
2018-06-26 12:29:26 -0700

    added account page, listing page, and edited almost all pages

--- public/assets/js/loginForm.js
+++ public/assets/js/loginForm.js
@@ -1,10 +1,20 @@
 // login form tabs
 $(".password-two-field").hide();
 
-$(".sign-up-tab").on("click", function(){
-	$(".password-two-field").show();
+const passwordtwo = $(".password-two-field");
+const password = $(".password-field");
+
+$(".sign-up-tab").on("click", function() {
+    $(".password-two-field").show();
 })
 
-$(".log-in-tab").on("click", function(){
-	$(".password-two-field").hide();
+$(".log-in-tab").on("click", function() {
+    $(".password-two-field").hide();
+})
+
+
+$(".login-button").on("click", () => {
+        if (password === null) {
+            console.log("field is required.")
+        }
 })
