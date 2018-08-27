1cc4116b8213a57b502aebd27286957ce75bd3bd
Lina Shamoon <shamoonlina@gmail.com>
2018-08-26 22:28:03 -0700

    moved search bar on map page

--- assets/js/loginForm.js
+++ assets/js/loginForm.js
@@ -1,20 +1,19 @@
 // login form tabs
 $(".password-two-field").hide();
+$(".signup-button").hide();
+$(".login-button").show();
 
 const passwordtwo = $(".password-two-field");
 const password = $(".password-field");
 
 $(".sign-up-tab").on("click", function() {
     $(".password-two-field").show();
+    $(".login-button").hide();
+    $(".signup-button").show();
 })
 
 $(".log-in-tab").on("click", function() {
     $(".password-two-field").hide();
+    $(".signup-button").hide();
+    $(".login-button").show();
 })
-
-
-// $(".login-button").on("click", () => {
-//         if (password === null) {
-//             console.log("field is required.")
-//         }
-// })
